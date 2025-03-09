7ee952bd0d5cc571329b8384930883d30b223f16
Torlens Joachim <torlens95@hotmail.com>
2025-03-09 20:15:47 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Trouves-tout",
+  description: "Un site comme Leboncoin où l'utilisateur a la capacité de créer, lire, modifier et supprimer une annonce, certaines de ses fonctionnalités nécessiteront d'être connecté, etc",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center">HomePage</h1>
+    </div>
+  );
+}
